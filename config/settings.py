68e468687f7c94a6782db9a0a1a19c26f822dfd9
ggoreb <seorab@naver.com>
2022-05-11 18:18:08 +0900

    오라클 설정 추가 - settings.py

--- config/settings.py
+++ config/settings.py
@@ -76,6 +76,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# Oracle
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.oracle',
+#         'NAME': 'ORCLHJ',
+#         'USER': 'system',
+#         'PASSWORD': '1234',
+#         'HOST': '127.0.0.1',
+#         'PORT': '1521',
+#     }
+# }
+# SQLite
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
