050344836d6b67e2d80bbedb9630b97b8e989f72
ggoreb <seorab@naver.com>
2022-05-11 15:16:55 +0900

    연습문제 secondapp 생성

--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,9 @@ urlpatterns = [
     path('index2/', views.index2),
     path('first/', include('firstapp.urls')),
     path('home/', config_views.home),
+
+    path('second/', include('secondapp.urls')),
+
+
+
 ]
