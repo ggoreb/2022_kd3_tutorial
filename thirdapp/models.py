65a785856afdca482039a7645a1b9c9be312245a
ggoreb <seorab@naver.com>
2022-05-12 15:18:49 +0900

    jeju olle 연습문제

--- thirdapp/models.py
+++ thirdapp/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+# from django.forms import CharField, IntegerField
+from django.db.models.fields import CharField, IntegerField, FloatField
+from django.db.models.fields import FloatField
+from django.db.models.fields import DateField
 
-from django.db import models
-from django.db.models.fields import CharField, IntegerField
+class JejuOlle(models.Model):
+  course = CharField(max_length=10)
+  course_name = CharField(max_length=20)
+  distance = FloatField()
+  time_info = CharField(max_length=10)
+  start_end_info = CharField(max_length=30)
+  cre_date = DateField()
+  class Meta:
+    db_table = 'jeju_olle'
+    managed = False
+
 class Shop(models.Model):
   shop_id = IntegerField(primary_key=True)
   shop_name = CharField(max_length=100, null=True)
@@ -9,7 +22,9 @@ class Shop(models.Model):
   rest_date = CharField(max_length=100, null=True)
   parking_info = CharField(max_length=100, null=True)
   img_path = CharField(max_length=255, null=True)
+
   class Meta:
     db_table = 'shop'
     app_label = 'thirdapp'
     managed = False
+
--- thirdapp/views.py
+++ thirdapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-from .models import Shop
+from .models import Shop, JejuOlle
+
+def jeju_olle(request):
+  jeju_olle_list = JejuOlle.objects.all()
+  print(jeju_olle_list)
+  return render(
+    request,
+    'thirdapp/jeju_olle.html',
+    {'jeju_olle_list': jeju_olle_list}
+  )
 
 def shop(request):
   shop_list = Shop.objects.all()
