b47401c3b0ffcd836b2832b3e268e992c5ff8664
ggoreb <seorab@naver.com>
2022-05-17 18:13:08 +0900

    course form widget 제거. 디자인 직접

--- secondapp/forms.py
+++ secondapp/forms.py
@@ -5,13 +5,5 @@ class CourseForm(forms.ModelForm):
   
   class Meta:
     model = Course
-    fields = ['name', 'cnt'] # id 속성은 PK 이므로 사용하지 않음
-    widgets = { # fields에 명시된 속성만 사용
-      'name': forms.TextInput(
-      attrs={'required': False, 'size': 10}),
-      'cnt': forms.TextInput(
-      attrs={'required': False, 'size': 10}),
-    }
-    labels = { # fields에 명시된 속성만 사용
-      'name': '과목', 'cnt': '수강인원'
-    }
+    fields = ['name', 'cnt']
+   
