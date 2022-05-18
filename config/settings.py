ab88c97cc66cf905998d9959bcd19a1445117c3e
ggoreb <seorab@naver.com>
2022-05-18 15:41:07 +0900

    form 사용 파일 업로드 db 자동 저장

--- config/settings.py
+++ config/settings.py
@@ -153,3 +153,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/member/login/'
 LOGOUT_REDIRECT_URL = '/member/login/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
